3160707a233dac629336ab02c45606559b3b7da8
odm275 <pomejia@gmail.com>
2022-09-21 10:46:20 -0500

    teset

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/odm275/mi-sitio',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/logo.png',
   email: 'oscarmejiaweb@gmail.com',
   github: 'https://github.com/odm275',
   // twitter: 'https://twitter.com/spaceicediver',
