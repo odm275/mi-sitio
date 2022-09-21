c207b766247d6c38ab45e51b066891a6de6ac3d3
odm275 <pomejia@gmail.com>
2022-09-20 20:39:16 -0500

    use my fullname and fix text

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'Mise - Fullstack Case Study',
-    description: `Case Study of a fullstack project done`,
+    description: `Candelaris and its businesses needed a comprehensive technology program to improve their inventory management, employee training, communication between managers and kitchens, and orders between the kitchen commissary and its locations. `,
     imgSrc: '/static/images/projects/mise/miseMainPic.png',
     href: '/blog/mise',
   },
